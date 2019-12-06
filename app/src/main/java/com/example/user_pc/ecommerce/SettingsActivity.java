04e662e008886ac019f5b76b71fdab2a58982877
Shaun99 <54528557+Shaun99@users.noreply.github.com>
2019-12-06 23:01:54 +0800

    first

--- app/src/main/java/com/example/user_pc/ecommerce/SettingsActivity.java
+++ app/src/main/java/com/example/user_pc/ecommerce/SettingsActivity.java
@@ -37,9 +37,6 @@ public class SettingsActivity extends AppCompatActivity {
     private EditText fullNameEditText, userPasswordEditText, addressEditText;
     private TextView profileChangeTextBtn,  closeTextBtn, saveTextButton;
 
-    private Uri imageUri;
-    private String myUrl = "";
-    private StorageTask uploadTask;
     private StorageReference storageProfilePrictureRef;
     private String checker = "";
 
@@ -55,12 +52,8 @@ public class SettingsActivity extends AppCompatActivity {
         fullNameEditText = (EditText) findViewById(R.id.settings_full_name);
         userPasswordEditText = (EditText) findViewById(R.id.settings_password);
         addressEditText = (EditText) findViewById(R.id.settings_address);
-        profileChangeTextBtn = (TextView) findViewById(R.id.profile_image_change_btn);
         closeTextBtn = (TextView) findViewById(R.id.close_settings_btn);
         saveTextButton = (TextView) findViewById(R.id.update_account_settings_btn);
-
-        //Pass the user inputs into this function to be stored in firebase database
-        userInfoDisplay(profileImageView, fullNameEditText, userPasswordEditText, addressEditText);
 
         //Close button function
         closeTextBtn.setOnClickListener(new View.OnClickListener() {
@@ -76,34 +69,37 @@ public class SettingsActivity extends AppCompatActivity {
             @Override
             public void onClick(View view)
             {
-                if (checker.equals("clicked"))
-                {   //Save all user info including user image profile
+                {   //save user text info
                     userInfoSaved();
                 }
-                else
-                {   //Only save user text info
-                    updateOnlyUserInfo();
-                }
             }
         });
 
-        //Update user profile function
-        profileChangeTextBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view)
-            {
-                checker = "clicked";
-
-                CropImage.activity(imageUri)
-                        .setAspectRatio(1, 1)
-                        .start(SettingsActivity.this);
-            }
-        });
     }
 
     private void close() {
         Toast.makeText(this, "No changes were made", Toast.LENGTH_LONG).show();
         finish();
+    }
+
+    private void userInfoSaved()
+    {   //Prompt error message if any of the required field is blank
+        if (TextUtils.isEmpty(fullNameEditText.getText().toString()))
+        {
+            Toast.makeText(this, "Please fill in the name.", Toast.LENGTH_SHORT).show();
+        }
+        else if (TextUtils.isEmpty(userPasswordEditText.getText().toString()))
+        {
+            Toast.makeText(this, "Please fill in the phone.", Toast.LENGTH_SHORT).show();
+        }
+        else if (TextUtils.isEmpty(addressEditText.getText().toString()))
+        {
+            Toast.makeText(this, "Please fill in the address.", Toast.LENGTH_SHORT).show();
+        }
+        else
+        {
+            updateOnlyUserInfo();
+        }
     }
 
     private void updateOnlyUserInfo()
@@ -121,145 +117,5 @@ public class SettingsActivity extends AppCompatActivity {
         finish();
     }
 
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data)
-    {
-        super.onActivityResult(requestCode, resultCode, data);
-
-        if (requestCode==CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE  &&  resultCode==RESULT_OK  &&  data!=null)
-        {   //Store the image Uri into the variable
-            CropImage.ActivityResult result = CropImage.getActivityResult(data);
-            imageUri = result.getUri();
-            //Display the image selected in the image view
-            profileImageView.setImageURI(imageUri);
-        }
-        else
-        {
-            Toast.makeText(this, "Error, Try Again.", Toast.LENGTH_SHORT).show();
-
-            startActivity(new Intent(SettingsActivity.this, SettingsActivity.class));
-            finish();
-        }
-    }
-
-
-    private void userInfoSaved()
-    {   //Prompt error message if any of the required field is blank
-        if (TextUtils.isEmpty(fullNameEditText.getText().toString()))
-        {
-            Toast.makeText(this, "Please fill in the name.", Toast.LENGTH_SHORT).show();
-        }
-        else if (TextUtils.isEmpty(userPasswordEditText.getText().toString()))
-        {
-            Toast.makeText(this, "Please fill in the phone.", Toast.LENGTH_SHORT).show();
-        }
-        else if (TextUtils.isEmpty(addressEditText.getText().toString()))
-        {
-            Toast.makeText(this, "Please fill in the address.", Toast.LENGTH_SHORT).show();
-        }
-        else if(checker.equals("clicked"))
-        {
-            uploadImage();
-        }
-    }
-    //Storing the user info inputs function
-    private void uploadImage()
-    {
-        final ProgressDialog progressDialog = new ProgressDialog(this);
-        progressDialog.setTitle("Update Profile");
-        progressDialog.setMessage("Please wait, while we are updating your account information");
-        progressDialog.setCanceledOnTouchOutside(false);
-        progressDialog.show();
-
-        if (imageUri != null)
-        {
-            final StorageReference fileRef = storageProfilePrictureRef
-                    .child(Prevalent.currentOnlineUser.getPhone() + ".jpg");
-            //Save the file to the storage
-            uploadTask = fileRef.putFile(imageUri);
-
-            uploadTask.continueWithTask(new Continuation() {
-                @Override
-                public Object then(@NonNull Task task) throws Exception
-                {
-                    if (!task.isSuccessful())
-                    {
-                        throw task.getException();
-                    }
-
-                    return fileRef.getDownloadUrl();
-                }
-            })
-                    .addOnCompleteListener(new OnCompleteListener<Uri>() {
-                        @Override
-                        public void onComplete(@NonNull Task<Uri> task)
-                        {
-                            if (task.isSuccessful())
-                            {   //Get the image URL and store inside the firebase database
-                                Uri downloadUrl = task.getResult();
-                                myUrl = downloadUrl.toString();
-                                //Create a database reference
-                                DatabaseReference ref = FirebaseDatabase.getInstance().getReference().child("Users");
-
-                                HashMap<String, Object> userMap = new HashMap<>();
-                                userMap. put("name", fullNameEditText.getText().toString());
-                                userMap. put("address", addressEditText.getText().toString());
-                                userMap. put("password", userPasswordEditText.getText().toString());
-                                userMap. put("image", myUrl);
-                                ref.child(Prevalent.currentOnlineUser.getPhone()).updateChildren(userMap);
-
-                                progressDialog.dismiss();
-
-                                startActivity(new Intent(SettingsActivity.this, MainActivity.class));
-                                Toast.makeText(SettingsActivity.this, "Profile Info update successfully.", Toast.LENGTH_SHORT).show();
-                                finish();
-                            }
-                            else
-                            {
-                                progressDialog.dismiss();
-                                Toast.makeText(SettingsActivity.this, "Error.", Toast.LENGTH_SHORT).show();
-                            }
-                        }
-                    });
-        }
-        else
-        {
-            Toast.makeText(this, "image is not selected.", Toast.LENGTH_SHORT).show();
-        }
-    }
-
-
-    private void userInfoDisplay(final CircleImageView profileImageView, final EditText fullNameEditText, final EditText userPhoneEditText, final EditText addressEditText)
-    {   //Create a database reference
-        DatabaseReference UsersRef = FirebaseDatabase.getInstance().getReference().child("Users").child(Prevalent.currentOnlineUser.getPhone());
-
-        UsersRef.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(DataSnapshot dataSnapshot)
-            {   //Check whether the user phone id exist
-                if (dataSnapshot.exists())
-                {   //Check whether the image exist
-                    if (dataSnapshot.child("image").exists())
-                    {   //Fetch and display the information of the user
-                        String image = dataSnapshot.child("image").getValue().toString();
-                        String name = dataSnapshot.child("name").getValue().toString();
-                        String password = dataSnapshot.child("password").getValue().toString();
-                        String address = dataSnapshot.child("address").getValue().toString();
-                        //Load the image and display into the profile view
-                        Picasso.get().load(image).into(profileImageView);
-                        fullNameEditText.setText(name);
-                        userPasswordEditText.setText(password);
-                        addressEditText.setText(address);
-                    }
-                }
-            }
-
-            @Override
-            public void onCancelled(DatabaseError databaseError) {
-
-            }
-        });
-    }
 }
 
